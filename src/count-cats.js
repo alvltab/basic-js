cbc6e94707bdf79832a84edbbc956f24c39cbef9
alvltab <alvltab@bk.ru>
2022-09-05 17:07:15 +0700

    feat: complete count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let counter = 0;
+  for (let row of backyard) {
+    for (let el of row) {
+      if (el === '^^') {
+        counter++;
+      }
+    }
+  }
+  return counter;
 }
 
 module.exports = {
