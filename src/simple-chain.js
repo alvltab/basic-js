fdae1f564c256703d265d6aec51b23350e8bd197
alvltab <alvltab@bk.ru>
2022-09-07 20:18:58 +0700

    feat: complete simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  links: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.links.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value = '') {
+    this.links.push(`${value} `);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  // А ЕСЛИ АРГУМЕНТ НЕ ПЕРЕДАН У ТЕБЯ БУДЕТ
+  // UNDEFINED А ДОЛЖНО БЫТЬ ПУСТОЕ ЗВЕНО!
+
+  removeLink(position) {
+/*
+    if (typeof(position) !== 'number' || position <= 0 || position > this.links.length) {
+      throw new Error("You can\'t remove incorrect link!");
+    } else {
+      this.links.splice(position-1, 1);
+      return this;
+    }
+*/
+    if (typeof(position) === 'number'
+        && position > 0
+        && position <= this.links.length) {
+      this.links.splice(position-1, 1);
+    } else {
+      this.links = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.links.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = '';
+    for (let link of this.links) {
+      result += `( ${link})~~`
+    }
+    this.links = [];
+    return result.slice(0, result.length-2);
   }
 };
 
