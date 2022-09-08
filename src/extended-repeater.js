80f72d4a343181758483507352e0e4fd8d036182
alvltab <alvltab@bk.ru>
2022-09-08 10:56:14 +0700

    feat: complete extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string = '';
+  if (str !== '') {
+    string = `${str}`;
+  }
+
+  let repeatTimes = 1;
+  if (options.hasOwnProperty('repeatTimes')) {
+    repeatTimes = options.repeatTimes;
+  }
+
+  let separator = '';
+  if (repeatTimes > 1) {
+    if (options.hasOwnProperty('separator')) {
+      separator = `${options.separator}`;
+    } else {
+      separator = '+';
+    }
+  }
+
+  let addition = '';
+  if (options.hasOwnProperty('addition')) {
+    addition = `${options.addition}`;
+  }
+
+  let additionRepeatTimes = 1;
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  
+  let additionSeparator = '';
+  if (additionRepeatTimes > 1) {
+    if (options.hasOwnProperty('additionSeparator')) {
+      additionSeparator = `${options.additionSeparator}`;
+    } else {
+      additionSeparator = '|';
+    }
+  }
+
+  let adder;
+  if (additionRepeatTimes > 1) {
+    adder = `${addition}`;
+  } else {
+    adder = `${addition}${additionSeparator}`;
+  }
+
+  for (let addRe = 1; addRe < additionRepeatTimes; addRe++) {
+    adder += additionSeparator + addition;
+  }
+  string += `${adder}`;
+
+  let result;
+  if (repeatTimes > 1) {
+    result = `${string}`;
+  } else {
+    result = `${string}${separator}`;
+  }
+  for (let strRe = 1; strRe < repeatTimes; strRe++) {
+    result += separator + string;
+  }
+  return result;
 }
 
 module.exports = {
